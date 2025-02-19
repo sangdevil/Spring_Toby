af891f0159f6ac4321a706b287c9e98c5f80a299
sangdevil <dnfzja1213@kaist.ac.kr>
2025-02-19 21:36:17 +0900

    IOC_134

--- src/main/java/ioc/config/FoolHelloService.java
+++ src/main/java/ioc/config/FoolHelloService.java
@@ -8,6 +8,11 @@ import org.springframework.context.annotation.Bean;
 public class FoolHelloService {
 
     // 스프링에서 @Configuration이 없는 경우 CGLIB 프록시를 사용하지 않아서,
+    // 메서드가 호출될 때마다 직접 새 인스턴스를 생성해서 반환하게 됨.
+    // 맨 처음 Printer를 빈으로 등록할 때 새 객체 생성
+    // 그 다음에 hello, hello2를 생성할 때 또 새 객체 생성.
+    // 또한, hello, hello2에 주입된 Printer 객체는 빈이 아니며,
+    // 스프링 컨테이너가 관리하지 않음.
 
     @Bean
     public Hello hello() {
